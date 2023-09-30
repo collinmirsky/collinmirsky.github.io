b1c969e87f799bdd30f9844410ff3b3c16b2155a
collinmirsky <collinimirsky@gmail.com>
2023-09-30 17:35:24 -0500

    More styling tweaks and js scroll nav

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+// Navbar Scroll style change
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.getElementById("navbar");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+});
+
 // Main JS
 const heroH1 = document.querySelector("#hero h1");
 
